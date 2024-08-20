b1603e4174919c83a28578546d4e1557df31b7dc
Nar Mena <narciris.tech@gmail.com>
2024-08-19 22:20:06 -0500

    subiendo metodo borrar

--- taskapplicationn/src/main/java/com/app/taskapplication/Controller/TaskController.java
+++ taskapplicationn/src/main/java/com/app/taskapplication/Controller/TaskController.java
@@ -9,10 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -66,6 +63,14 @@ public class TaskController {
         logger.info("task update" + task);
         serviceTask.saveTask(task);
         return "redirect:/"; // redirigimos al path de inicio
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable(value = "id") Long id){
+        Task task = new Task();
+        task.setId(id);
+        serviceTask.deleteTask(task);
+        return "redirect:/";
     }
 
 }
